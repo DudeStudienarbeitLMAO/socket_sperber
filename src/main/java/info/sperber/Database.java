6b7fd9ee9565ca6e24fb911e2b0b2af9e95e29ee
faxe1008 <vielspeicherplatz6@gmail.com>
2018-03-08 14:48:50 +0100

    orm maven stuff

---
+++ src/main/java/info/sperber/Database.java
@@ -0,0 +1,19 @@
+package info.sperber;
+
+import java.sql.*;
+
+/**
+ * Created by fabian on 3/8/18.
+ */
+public class Database {
+
+
+    public static void main(String[] args) throws Exception{
+        Class.forName("com.mysql.jdbc.Driver").newInstance();
+        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/vorlesung?user=vorlesung&password=vorlesung");
+
+
+
+    }
+
+}
